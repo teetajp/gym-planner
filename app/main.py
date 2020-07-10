a0689b33392173baf64f5aa468d3d7f0b7854c26
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-10 16:49:48 +0700

    Fixed typo in main.py

--- app/main.py
+++ app/main.py
@@ -43,5 +43,5 @@ def register():
 def favicon(): 
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ = '__main__':
-    app.run()
+if __name__ == "__main__": 
+        app.run() 
