29c9e3562bc3e11ba0d177052a9cac8650e3d228
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-10 16:22:18 +0700

    Added bootstrap and favicon

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+import os
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, send_from_directory
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from helpers import apology, login_required
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route('/')
 def index():
-    return 'Index Page'
+    return render_template("index.html")
 
 @app.route('/user_login')
 def user_login():
@@ -21,3 +38,7 @@ def coach_login():
 def register():
     """Allow coaches and users to register an account"""
     return 'TODO'
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
--- runserver.ps1
+++ runserver.ps1
@@ -1,4 +1,4 @@
 venv\Scripts\activate.ps1
 $env:FLASK_APP="app\main.py"
 $env:FLASK_ENV="development"
-flask run --host=0.0.0.0
+flask run
