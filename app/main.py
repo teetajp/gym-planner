da6aef4d8adf4d4e2cfaa6221b9d187b7947150a
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-10 14:59:33 +0700

    Added helpers.py from CS50 Finance

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from helpers import apology, login_required
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-    
+def index():
+    return 'Index Page'
+
+@app.route('/user_login')
+def user_login():
+    """Log user in"""
+    return 'Users can login here'
+
+@app.route('/coach_login')
+def coach_login():
+    """Log coach in"""
+    return 'Coaches can login here'
+
+@app.route('/register')
+def register():
+    """Allow coaches and users to register an account"""
+    return 'TODO'
