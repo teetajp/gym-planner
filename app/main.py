fd116a808e32205d74d27559ade3aec5d708faf3
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-10 16:47:25 +0700

    Added new Procfile

--- app/main.py
+++ app/main.py
@@ -42,3 +42,6 @@ def register():
 @app.route('/favicon.ico') 
 def favicon(): 
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ = '__main__':
+    app.run()
