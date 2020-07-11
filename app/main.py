7104e452561eaa919de1417ce538ca19f5224bd5
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-11 13:24:49 +0700

    Added flask-session and added a few to-do functions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, send_from_directory
+
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -20,28 +22,64 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route('/')
+@login_required
 def index():
     return render_template("layout.html")
 
 @app.route('/user_login')
 def user_login():
     """Log user in"""
-    return 'Users can login here'
+    return render_template("layout.html")
 
 @app.route('/coach_login')
 def coach_login():
     """Log coach in"""
-    return 'Coaches can login here'
+    return 'TODO'
 
-@app.route('/register')
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """Allow coaches and users to register an account"""
-    return 'TODO'
+    
+    return "TODO"
+
+@app.route("/change_pw", methods=["GET", "POST"])
+@login_required
+def change_pw():
+    """Allow user to change password"""
+
+    return "TODO"
 
 @app.route('/favicon.ico') 
 def favicon(): 
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
 
 if __name__ == "__main__": 
         app.run() 
