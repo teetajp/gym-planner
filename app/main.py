f586a55aee929acd4206d4bab0f0c0633aac8819
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-11 22:03:56 +0700

    Format files using Black

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import os
-from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, send_from_directory
+from flask import (
+    Flask,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    send_from_directory,
+)
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -14,11 +24,11 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Make sure DATABASE_URL key is set
-if not os.environ.get("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL not set")
+# # Make sure DATABASE_URL key is set
+# if not os.environ.get("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL not set")
 
-
+print(os.getenv("DATABASE_URL"))
 # Connect to PostgreSQL database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -31,18 +41,21 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route('/')
+
+@app.route("/")
 @login_required
 def index():
     return render_template("layout.html")
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
     """Log user and/or coach in"""
     return render_template("login.html")
@@ -58,11 +71,13 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Allow coaches and users to register an account"""
-    
+
     return render_template("register.html")
+
 
 @app.route("/change_pw", methods=["GET", "POST"])
 @login_required
@@ -71,9 +86,15 @@ def change_pw():
 
     return "TODO"
 
-@app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
 
 def errorhandler(e):
     """Handle error"""
@@ -87,5 +108,6 @@ def errorhandler(e):
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
-if __name__ == "__main__": 
-        app.run() 
+if __name__ == "__main__":
+    app.run()
+
