789d1e5ba4f531c890ad68c3c6d97cc9672a568a
Teetaj Pavaritpong <teetajp@gmail.com>
2020-07-13 18:17:42 +0700

    Add DB functionality to register.html and login.html (#3)
    * 🔨 Create persons table in local DB
    
    * Add runserver script
    
    * 🔨 Modify persons table and insert dummy data
    
    Also pushed local DB on to Heroku Postgres
    
    * 🗃 Change user/coach table structures and insert dummy data and test selections
    🔥 Renamed some SQL query files
    
    * Edit PSQL scripts
    
    * Add .env dependency and edited register.html
    
    * ✨ Add login feature and allow users who register an account to be automatically logged in
    
    * Edit SQL commands to reset table for Heroku

--- app/main.py
+++ app/main.py
@@ -24,14 +24,6 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Make sure DATABASE_URL key is set\
-if not os.environ.get("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL not set")
-
-# Connect to PostgreSQL database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
@@ -39,6 +31,14 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+    
+# Make sure DATABASE_URL key is set
+if not os.environ.get("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL not set")
+
+# Connect to PostgreSQL database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -54,10 +54,47 @@ def index():
     return render_template("layout.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user and/or coach in"""
-    return render_template("login.html")
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        users = db.execute("SELECT user_id, hash FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()
+
+        # Ensure username exists
+        if len(users) != 1:
+            return apology("invalid username and/or password", 403)
+
+        # Ensure submitted password matches password in database
+        for user_id, hash in users:
+            if not check_password_hash(hash, request.form.get("password")):
+                return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = user_id
+
+        # Redirect user to home page
+        flash("Login successful!")
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
 @app.route("/logout")
@@ -75,7 +112,48 @@ def logout():
 def register():
     """Allow coaches and users to register an account"""
 
-    return render_template("register.html")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure username does not already exist
+        elif len(db.execute("SELECT user_id FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()) != 0:
+            return apology("username already exists", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure password and confirmation matches
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 403)
+
+        # TODO: Add regex filter to sanitize email inputs
+
+        # Insert the new user into users table
+        db.execute("INSERT INTO users (username, hash, email) VALUES (:username, :hash, :email)",
+            {"username": request.form.get("username"),
+            "hash": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email")})
+        print(f"Added user with username: " + request.form.get("username") +
+        " and email: " + request.form.get("email"))
+        db.commit()
+
+        # Log registered user in and remember the id
+        session["user_id"] = db.execute("SELECT user_id FROM users WHERE username = :username",
+                          {"username": request.form.get("username")}).fetchall()[0]
+
+        # Redirect user to home page
+        flash("Registration successful. Automatically logged in.")
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 @app.route("/change_pw", methods=["GET", "POST"])
